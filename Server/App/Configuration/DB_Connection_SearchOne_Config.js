50f80002dd9b558661e4889605fca5a5dadbaad9
NandaLochana <danushkanandalochana@gmail.com>
2024-01-30 11:35:25 +0200

    Define Framewrork-01

---
+++ Server/App/Configuration/DB_Connection_SearchOne_Config.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri);
+const database = client.db('moviebooking');
+ function searchQuery(){
+    async function run() {
+        try {
+          const movies = database.collection('LoginInfo');
+          const query = { title: 'Back to the Future' };
+           const movie = await movies.findOne(query);
+           console.log(movie)
+        }
+         finally {
+          // Ensures that the client will close when you finish/error
+          await client.close();
+        }
+      }
+      run().catch(console.dir);
+}
+
+module.exports = {
+  searchQuery
+}
